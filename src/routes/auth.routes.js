6ad3663351c10f24aabfd5ca3b8d8c7a28b75932
Edwin <e.devia.b1306@gmail.com>
2026-05-24 07:37:47 -0500

    Validaciones de datos a registro y login.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,11 +3,13 @@ import {Router} from 'express';
 //se importan funciones desde el controller para el manejo de las rutas
 import {login, logout, profile, register} from "../controllers/auth.controller.js";
 import {authRequired} from "../middlewares/validateToken.js";
+import {validateSchema} from "../middlewares/validator.middleware.js";
+import {registerSchema, loginSchema} from "../schemas/auth.schema.js";
 
 const router = Router();
 //Se crean las rutas
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateSchema(registerSchema), register);
+router.post('/login', validateSchema(loginSchema), login);
 router.post('/logout', logout);
 router.get('/profile', authRequired, profile)
 
